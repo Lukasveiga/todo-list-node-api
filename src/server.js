bb3d52e36c5039b8073b24cf79e7ae69f7b677a4
Lukas Veiga <lukas.veiga@hotmail.com>
2023-07-28 19:03:54 -0300

    Refactor: Create middleware folder & Create router script and implement a endpoint test and Connect with controller layer

--- src/server.js
+++ src/server.js
@@ -1,17 +1,15 @@
 const express = require("express");
-const middleware = require("./middlewares");
+const middleware = require("./middlewares/index");
+const router = require("./routes/index");
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
-
 app.use(middleware.requestLog);
 
-app.get("/test", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/api/v1", router);
 
 app.listen(PORT, () => {
   console.log(`Express server listening on ${PORT}`);
