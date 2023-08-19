46624e6f4a24b6c8244b2b51e28ea5786de18c0c
Lukas Veiga <lukas.veiga@hotmail.com>
2023-08-19 11:01:35 -0300

    Refactor & Feat: Refactoring models folder and implements the relationship between user and task models

---
+++ src/model/task.js
@@ -0,0 +1,46 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const User = require("./user");
+const database = require("../database/connect");
+
+const Task = database.define("tb_task", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: Date.now(),
+  },
+  finishedAt: {
+    type: DataTypes.DATE,
+  },
+  finished: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+  priority: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+  },
+});
+
+// ORM
+Task.belongsTo(User, {
+  constraint: true,
+  foreignKey: "id_user",
+});
+
+User.hasMany(Task, {
+  foreignKey: "id_user",
+});
+
+module.exports = Task;
---
+++ src/model/user.js
@@ -0,0 +1,20 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const database = require("../database/connect");
+
+const User = database.define("tb_user", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = User;
