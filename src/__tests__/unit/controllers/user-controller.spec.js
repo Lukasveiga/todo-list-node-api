199998228bf61c962cf34b2f117e4a3157f8f284
Lukas Veiga <lukas.veiga@hotmail.com>
2023-09-26 09:51:12 -0300

    create helper factory methods"

--- src/__tests__/unit/controllers/user-controller.spec.js
+++ src/__tests__/unit/controllers/user-controller.spec.js
@@ -1,3 +1,17 @@
+const UserController = require("../../../controllers/user-controller");
+
+const makeUserServiceSpy = () => {
+  class UserServiceSpy {}
+
+  return new UserServiceSpy();
+};
+
+const makeSut = () => {
+  const userServiceSpy = makeUserServiceSpy();
+  const sut = new UserController(userServiceSpy);
+  return { sut, userServiceSpy };
+};
+
 describe("User Controller", () => {
   test("", () => {});
 });
