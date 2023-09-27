829872854cee008457a9f30797972b6b6afd9235
Lukas Veiga <lukas.veiga@hotmail.com>
2023-09-27 09:21:51 -0300

    uncouple database configurations from user model

--- src/database/model/user.js
+++ src/database/model/user.js
@@ -1,29 +1,36 @@
 const { DataTypes } = require("sequelize");
-const database = require("../connect");
 
-const User = database.define(
-  "tb_user",
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  },
-  { timestamps: false }
-);
+class UserModel {
+  constructor(database) {
+    this.database = database;
+  }
 
-module.exports = User;
+  generateModel(database) {
+    return database.define(
+      "tb_user",
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        username: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        email: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: true,
+        },
+        password: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+      },
+      { timestamps: false }
+    );
+  }
+}
+
+module.exports = UserModel;
