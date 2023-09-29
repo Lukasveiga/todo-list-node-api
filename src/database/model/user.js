9d7079614d6688f86c1129c2df53a44dd905257b
Lukas Veiga <lukas.veiga@hotmail.com>
2023-09-28 22:45:17 -0300

    remove user model class

--- src/database/model/user.js
+++ src/database/model/user.js
@@ -1,39 +1,29 @@
 const { DataTypes } = require("sequelize");
 const database = require("../connect");
 
-class UserModel {
-  constructor(database) {
-    this.database = database;
-  }
+const User = database.define(
+  "tb_user",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  { timestamps: false }
+);
 
-  generateModel() {
-    return this.database.define(
-      "tb_user",
-      {
-        id: {
-          type: DataTypes.INTEGER,
-          autoIncrement: true,
-          primaryKey: true,
-        },
-        username: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-        email: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          unique: true,
-        },
-        password: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-      },
-      { timestamps: false }
-    );
-  }
-}
-
-const userModel = new UserModel(database);
-
-module.exports = userModel;
+module.exports = User;
