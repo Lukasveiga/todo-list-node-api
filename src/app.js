d7277cdfa4ff4da4c49b0b3ad505e00639e0339d
Lukas Veiga <lukas.veiga@hotmail.com>
2023-10-03 20:46:25 -0300

    Add helmet package

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const { requestLog } = require("./middlewares/index");
 const router = require("./routes/index");
+require("express-async-errors");
+const helmet = require("helmet");
 
 const app = express();
+app.use(helmet());
 
 app.use(express.json());
 app.use(requestLog);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,1 @@
-const { Router } = require("express");
-const controller = require("../controllers/index");
 
-const router = new Router();
-
-router.get("/", controller.test);
-
-module.exports = router;
