3d3b6dff0691cb8b708f4e0c48a1b4a948f68015
Lukas Veiga <lukas.veiga@hotmail.com>
2023-10-10 10:28:58 -0300

    config enviroments

--- src/database/connect.js
+++ src/database/connect.js
@@ -5,13 +5,26 @@ let sequelize;
 
 if (process.env.NODE_ENV === "dev") {
   sequelize = new Sequelize(
+    process.env.DB_NAME_DEV,
+    process.env.DB_USER_DEV,
+    process.env.DB_PASSWORD_DEV,
+    {
+      dialect: "postgresql",
+      host: process.env.DB_HOST_DEV,
+      port: process.env.DB_PORT_DEV,
+    }
+  );
+}
+
+if (process.env.NODE_ENV === "prod") {
+  sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
       dialect: "postgresql",
       host: process.env.DB_HOST,
-      port: 5432,
+      port: process.env.DB_PORT,
     }
   );
 }
