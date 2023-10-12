a351fbe4588637e7a983747f0030926212cefd06
Lukas Veiga <lukas.veiga@hotmail.com>
2023-10-12 14:44:25 -0300

    ensure UserController return status code 400 when provided invalid params

--- src/__tests__/integration/user-controller.test.js
+++ src/__tests__/integration/user-controller.test.js
@@ -48,4 +48,67 @@ describe("User Controller", () => {
     expect(response.status).toBe(400);
     expect(response.body.message).toBe("User already exists.");
   });
+
+  test("should return status code 400 when is not provided a param", async () => {
+    const userCases = [].concat(
+      {},
+      {
+        username: "any_username",
+      },
+      {
+        email: "any_email@email.com",
+      },
+      {
+        password: "any_password",
+      },
+      {
+        username: "any_username",
+        email: "any_email@email.com",
+      },
+      {
+        username: "any_username",
+        password: "any_password",
+      },
+      {
+        email: "any_email@email.com",
+        password: "any_password",
+      }
+    );
+
+    for (const userCase of userCases) {
+      const response = await request(app).post("/api/v1/user").send(userCase);
+
+      expect(response.status).toBe(400);
+      expect(response.body.message).toBe(
+        "All fields are required (username, email and password)"
+      );
+    }
+  });
+
+  test("should return status code 400 when invalid email is provided", async () => {
+    const userTest = {
+      username: "valid_username2",
+      email: "invalid_email",
+      password: "valid_password",
+    };
+    const response = await request(app).post("/api/v1/user").send(userTest);
+
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBe("Invalid email");
+  });
+
+  test("should return status code 400 when invalid password is provided", async () => {
+    const invalid_password = "123";
+    const userTest = {
+      username: "valid_username2",
+      email: "any_email@email.com",
+      password: invalid_password,
+    };
+    const response = await request(app).post("/api/v1/user").send(userTest);
+
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBe(
+      "Password must be at least 5 characters long"
+    );
+  });
 });
