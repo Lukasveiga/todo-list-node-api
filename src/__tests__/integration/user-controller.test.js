c4eebc320e721a5d2a61aae13322875e7b4d61c1
Lukas Veiga <lukas.veiga@hotmail.com>
2023-10-11 22:16:05 -0300

    first integration test with supertest

--- src/__tests__/integration/user-controller.test.js
+++ src/__tests__/integration/user-controller.test.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
-const app = require("../../app");
+const { app } = require("../../app");
+const request = require("supertest");
 const sequelize = require("../../database/connect");
 
 describe("User Controller", () => {
@@ -8,5 +9,19 @@ describe("User Controller", () => {
     await sequelize.sync({ force: true });
   });
 
-  test("", () => {});
+  test("should create new user and return status 201", async () => {
+    const userTest = {
+      username: "valid_username",
+      email: "valid_email@email.com",
+      password: "valid_password",
+    };
+    const response = await request(app).post("/api/v1/user").send(userTest);
+
+    delete userTest.password;
+
+    const { username, email } = response.body;
+
+    expect(response.status).toBe(201);
+    expect({ username, email }).toEqual(userTest);
+  });
 });
