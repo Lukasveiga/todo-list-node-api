b8cae1e211195c855a2eb68e5ead17bfb8cd1419
Lukas Veiga <lukas.veiga@hotmail.com>
2023-10-11 22:14:56 -0300

    remove compostion root for user route to its own folder/file

--- src/routes/user-route.js
+++ src/routes/user-route.js
@@ -1,21 +1,18 @@
 const { Router } = require("express");
+const userController = require("../composition-root/user-composition");
 const { requestBodyValidation } = require("../middlewares");
 const postUserSchema = require("../utils/validation/user/post-user-schema");
 const putUserSchema = require("../utils/validation/user/put-user-schema");
 
-const UserRepository = require("../repositories/user-repository");
-const Encrypter = require("../utils/encrypter");
-const UserService = require("../services/user-service");
-const UserController = require("../controllers/user-controller");
-
-const userService = new UserService(new UserRepository(), new Encrypter());
-const userController = new UserController(userService);
-
 const router = new Router();
 
-router.post("/", requestBodyValidation(postUserSchema), userController.create);
-router.get("/", userController.detailUser);
-router.put("/", requestBodyValidation(putUserSchema), userController.update);
-router.delete("/", userController.delete);
+router.post("/", requestBodyValidation(postUserSchema), (req, res) =>
+  userController.create(req, res)
+);
+router.get("/", (req, res) => userController.detailUser(req, res));
+router.put("/", requestBodyValidation(putUserSchema), (req, res) =>
+  userController.update(req, res)
+);
+router.delete("/", (req, res) => userController.delete(req, res));
 
 module.exports = router;
