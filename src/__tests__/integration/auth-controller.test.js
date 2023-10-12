cd0ecf129c4bbdd9cb995633e018c45e8706c057
Lukas Veiga <lukas.veiga@hotmail.com>
2023-10-12 17:19:08 -0300

    Ensure AuthController returns 400 when empty username or email is provided

--- src/__tests__/integration/auth-controller.test.js
+++ src/__tests__/integration/auth-controller.test.js
@@ -40,7 +40,7 @@ describe("Auth Controller", () => {
     expect(response.body.token).not.toBeNull();
   });
 
-  test("Should return 400 when invalid username are provided", async () => {
+  test("Should return 404 when invalid username are provided", async () => {
     const login = {
       username: "invalid_username",
       password: "any_password",
@@ -52,7 +52,7 @@ describe("Auth Controller", () => {
     expect(response.body.message).toBe("User not found");
   });
 
-  test("Should return 400 when invalid email are provided", async () => {
+  test("Should return 404 when invalid email are provided", async () => {
     const login = {
       email: "invalid_email@email.com",
       password: "any_password",
@@ -99,14 +99,21 @@ describe("Auth Controller", () => {
     expect(response.body.message).toBe("You must enter with username or email");
   });
 
-  test("Should return 400 when password is not provided", async () => {
-    const login = {
-      username: "any_username",
-    };
+  test("Should return 400 when empty username or email is provided", async () => {
+    const loginCases = [].concat(
+      { email: "", password: "any_password" },
+      { username: "", password: "any_password" }
+    );
 
-    const response = await request(app).post("/api/v1/login").send(login);
+    const fields = ["Email", "Username"];
+    let index = 0;
 
-    expect(response.status).toBe(400);
-    expect(response.body.message).toBe("Password must be provided");
+    for (const loginCase of loginCases) {
+      const response = await request(app).post("/api/v1/login").send(loginCase);
+
+      expect(response.status).toBe(400);
+      expect(response.body.message).toBe(`${fields[index]} cannot be empty`);
+      index++;
+    }
   });
 });
