79868af7fd05807a8e2030f2b23561cbe824c53c
Lukas Veiga <lukas.veiga@hotmail.com>
2023-10-12 14:30:01 -0300

    ensure UserController return 400 status code to username and email already existing in database

--- src/__tests__/integration/user-controller.test.js
+++ src/__tests__/integration/user-controller.test.js
@@ -24,4 +24,28 @@ describe("User Controller", () => {
     expect(response.status).toBe(201);
     expect({ username, email }).toEqual(userTest);
   });
+
+  test("should return status code 400 for username already exist", async () => {
+    const userTest = {
+      username: "valid_username",
+      email: "valid_email2@email.com",
+      password: "valid_password",
+    };
+    const response = await request(app).post("/api/v1/user").send(userTest);
+
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBe("User already exists.");
+  });
+
+  test("should return status code 400 for email already exist", async () => {
+    const userTest = {
+      username: "valid_username2",
+      email: "valid_email@email.com",
+      password: "valid_password",
+    };
+    const response = await request(app).post("/api/v1/user").send(userTest);
+
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBe("User already exists.");
+  });
 });
