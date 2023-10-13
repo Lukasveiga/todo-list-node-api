0094f2334e8642b94f70f4edaac8d993825b2824
Lukas Veiga <lukas.veiga@hotmail.com>
2023-10-13 09:55:16 -0300

    add validateAccessToken middleware to user route

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,5 +1,11 @@
 const requestLog = require("./request-log");
 const errorHandler = require("./error-handler");
 const requestBodyValidation = require("./request-body-validation");
+const validateAcessToken = require("./validate-access-token");
 
-module.exports = { requestLog, errorHandler, requestBodyValidation };
+module.exports = {
+  requestLog,
+  errorHandler,
+  requestBodyValidation,
+  validateAcessToken,
+};
--- src/routes/user-route.js
+++ src/routes/user-route.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const userController = require("../composition-root/user-composition");
-const { requestBodyValidation } = require("../middlewares");
+const { requestBodyValidation, validateAcessToken } = require("../middlewares");
 const postUserSchema = require("../utils/validation/user/post-user-schema");
 const putUserSchema = require("../utils/validation/user/put-user-schema");
 
@@ -9,6 +9,7 @@ const router = new Router();
 router.post("/", requestBodyValidation(postUserSchema), (req, res) =>
   userController.create(req, res)
 );
+router.use(validateAcessToken);
 router.get("/", (req, res) => userController.detailUser(req, res));
 router.put("/", requestBodyValidation(putUserSchema), (req, res) =>
   userController.update(req, res)
