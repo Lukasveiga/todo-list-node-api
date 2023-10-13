984688755005b5ef2750c29d799a289b8d12d214
Lukas Veiga <lukas.veiga@hotmail.com>
2023-10-13 20:09:07 -0300

    Ensure UserRepository return user body when find user by id

--- src/__tests__/unit/repositories/user-repository.spec.js
+++ src/__tests__/unit/repositories/user-repository.spec.js
@@ -4,19 +4,25 @@ const sequelize = require("../../../database/connect");
 
 const userRepository = new UserRepository();
 
+const userTest = {
+  username: "valid_username",
+  email: "valid_email@email.com",
+  password: "valid_password",
+};
+
 describe("User Repository", () => {
   afterAll(async () => {
     await sequelize.sync({ force: true });
   });
 
   test("Should return user body when created a new user", async () => {
-    const userTest = {
-      username: "valid_username",
-      email: "valid_email@email.com",
-      password: "valid_password",
-    };
-
     const user = await userRepository.create(userTest);
+    delete user.id;
+    expect(user).toEqual(userTest);
+  });
+
+  test("Should return user body when find a user by id", async () => {
+    const user = await userRepository.findById(1);
     delete user.id;
     expect(user).toEqual(userTest);
   });
