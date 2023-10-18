c4aad58d02e5897f9d63f9a523c642d353971327
Lukas Veiga <lukas.veiga@hotmail.com>
2023-10-18 15:12:29 -0300

    add update method to TaskService

--- src/services/task-service.js
+++ src/services/task-service.js
@@ -1,3 +1,5 @@
+const { NotFoundError, BadRequestError } = require("../utils/exceptions");
+
 class TaskService {
   constructor(taskRepository, cacheStorage) {
     this.taskRepository = taskRepository;
@@ -15,6 +17,51 @@ class TaskService {
     await this.cacheStorage.setStaleStatus(`findAll(${userId}):stale`, "true");
 
     return newTask;
+  }
+
+  async update(body, taskId, userId) {
+    const task = await this.taskRepository.findById(taskId);
+
+    if (!task) {
+      throw new NotFoundError("Task not found.");
+    }
+
+    const { title, description, priority, finished } = body;
+
+    const updateParams = {};
+
+    if (title) {
+      updateParams.title = title;
+    }
+
+    if (description) {
+      updateParams.description = description;
+    }
+
+    if (priority) {
+      updateParams.priority = priority;
+    }
+
+    if (finished !== null) {
+      updateParams.finished = finished;
+      if (finished) {
+        updateParams.finishedAt = Date.now();
+      }
+    }
+
+    const updatedTask = await this.taskRepository.update(
+      updateParams,
+      taskId,
+      userId
+    );
+
+    if (!updatedTask) {
+      throw new BadRequestError("Was not possible to update task.");
+    }
+
+    await this.cacheStorage.setStaleStatus(`findAll(${userId}):stale`, "true");
+
+    return updatedTask;
   }
 }
 
