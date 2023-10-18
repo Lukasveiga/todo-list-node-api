8627e437bb1c681f8110f29446b7ac21346bbb7f
Lukas Veiga <lukas.veiga@hotmail.com>
2023-10-18 14:48:04 -0300

    add create method to TaskService

--- src/services/task-service.js
+++ src/services/task-service.js
@@ -3,6 +3,19 @@ class TaskService {
     this.taskRepository = taskRepository;
     this.cacheStorage = cacheStorage;
   }
+
+  async create(body, userId) {
+    const { title, description, priority } = body;
+
+    const newTask = await this.taskRepository.create(
+      { title, description, priority },
+      userId
+    );
+
+    await this.cacheStorage.setStaleStatus(`findAll(${userId}):stale`, "true");
+
+    return newTask;
+  }
 }
 
 module.exports = TaskService;
