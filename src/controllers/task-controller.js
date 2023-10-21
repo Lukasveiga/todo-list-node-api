cc2148a6f53de4fd3c32130d13a72553af8859b2
Lukas Veiga <lukas.veiga@hotmail.com>
2023-10-21 20:37:57 -0300

    Add update and delete method to TaskController layer

--- src/controllers/task-controller.js
+++ src/controllers/task-controller.js
@@ -14,6 +14,29 @@ class TaskController {
 
     return res.status(201).json(newTask);
   }
+
+  async update(req, res) {
+    const { id } = req.user;
+    const { taskId } = req.params;
+    const { title, description, priority, finished } = req.body;
+
+    const updatedTask = await this.taskService.update(
+      { title, description, priority, finished },
+      taskId,
+      id
+    );
+
+    return res.status(200).json(updatedTask);
+  }
+
+  async delete(req, res) {
+    const { id } = req.user;
+    const { taskId } = req.params;
+
+    await this.taskService.delete(taskId, id);
+
+    return res.status(204).send();
+  }
 }
 
 module.exports = TaskController;
