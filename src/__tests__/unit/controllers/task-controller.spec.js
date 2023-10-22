ed8923d2f3c3f7a4211442ffd68baa4b72491168
Lukas Veiga <lukas.veiga@hotmail.com>
2023-10-21 22:24:54 -0300

    Ensure TaskController returns status code 204 no body content when is deleted a task

--- src/__tests__/unit/controllers/task-controller.spec.js
+++ src/__tests__/unit/controllers/task-controller.spec.js
@@ -19,6 +19,8 @@ const makeTaskServiceSpy = () => {
     async update(body, taskId, userId) {
       return this.serviceTaskTest;
     }
+
+    async delete(taskId, userId) {}
   }
 
   const taskServiceSpy = new TaskServiceSpy();
@@ -78,5 +80,29 @@ describe("Task Controller", () => {
 
     expect(res.status.calledWith(200)).to.be.true;
     expect(res.json.calledWith(taskTest)).to.be.true;
+  });
+
+  test("Should return status code 204 and no body content when is deleted a task", async () => {
+    const res = {
+      status: sinon.stub().returnsThis(),
+      send: sinon.spy(),
+    };
+
+    const req = {
+      user: {
+        id: 1,
+      },
+      params: {
+        taskId: 1,
+      },
+    };
+
+    const { sut } = makeSut();
+
+    await sut.delete(req, res);
+
+    expect(res.status.calledWith(204)).to.be.true;
+    expect(res.send.calledOnce).to.be.true;
+    expect(res.send.calledWith()).to.be.true;
   });
 });
