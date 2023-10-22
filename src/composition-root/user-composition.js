5bdf3477c7145309b12fb66a4985111f27ffd7da
Lukas Veiga <lukas.veiga@hotmail.com>
2023-10-22 11:43:55 -0300

    refactor user composition root

--- src/composition-root/user-composition.js
+++ src/composition-root/user-composition.js
@@ -3,9 +3,7 @@ const Encrypter = require("../utils/encrypter");
 const UserService = require("../services/user-service");
 const UserController = require("../controllers/user-controller");
 
-const encrypter = new Encrypter();
-const userRepository = new UserRepository();
-const userService = new UserService(userRepository, encrypter);
+const userService = new UserService(new UserRepository(), new Encrypter());
 const userController = new UserController(userService);
 
 module.exports = userController;
