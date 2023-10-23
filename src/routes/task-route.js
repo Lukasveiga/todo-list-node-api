f10255e2fafa7e4081fb89d7648a5193b9274453
Lukas Veiga <lukas.veiga@hotmail.com>
2023-10-23 09:29:01 -0300

    add schema validation to task route

--- src/routes/task-route.js
+++ src/routes/task-route.js
@@ -1,14 +1,25 @@
 const { Router } = require("express");
 const taskController = require("../composition-root/task-composition");
-const { validateAcessToken, urlParamValidation } = require("../middlewares");
+const {
+  validateAcessToken,
+  urlParamValidation,
+  requestBodyValidation,
+} = require("../middlewares");
+const postTaskSchema = require("../utils/validation/task/post-task-schema");
+const putTaskSchema = require("../utils/validation/task/put-task-schema");
 
 const router = new Router();
 
 router.use(validateAcessToken);
-router.post("/", (req, res) => taskController.create(req, res));
+router.post("/", requestBodyValidation(postTaskSchema), (req, res) =>
+  taskController.create(req, res)
+);
 router.get("/", (req, res) => taskController.findAll(req, res));
-router.put("/:taskId", urlParamValidation, (req, res) =>
-  taskController.update(req, res)
+router.put(
+  "/:taskId",
+  urlParamValidation,
+  requestBodyValidation(putTaskSchema),
+  (req, res) => taskController.update(req, res)
 );
 router.delete("/:taskId", urlParamValidation, (req, res) =>
   taskController.delete(req, res)
