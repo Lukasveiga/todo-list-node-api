df28731b61c5318b56a652bc3577be9eb2582c08
Lukas Veiga <lukas.veiga@hotmail.com>
2023-10-25 22:43:43 -0300

    Add swagger documentation

--- src/app.js
+++ src/app.js
@@ -6,12 +6,17 @@ require("express-async-errors");
 const { requestLog, errorHandler } = require("./middlewares");
 const { userRoute, authRoute, taskRoute } = require("./routes");
 
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("../swagger.json");
+
 const app = express();
 
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(requestLog);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/login", authRoute);
